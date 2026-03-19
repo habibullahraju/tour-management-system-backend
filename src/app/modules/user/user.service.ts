067468db11dbec3e9faa42b25a195f5a30d1b5a3
habibullahraju <raju.ra423@gmail.com>
2026-03-19 23:40:56 +0600

    create user service layar

---
+++ src/app/modules/user/user.service.ts
@@ -0,0 +1,16 @@
+import { IUser } from "./user.interface";
+import { User } from "./user.model";
+
+const createUser = async (payload: Partial<IUser>) => {
+  const userData: Partial<IUser> = {};
+
+  if (payload.name !== undefined) userData.name = payload.name;
+  if (payload.email !== undefined) userData.email = payload.email;
+  const user = await User.create(userData);
+
+  return user;
+};
+
+export const UserServices = {
+  createUser,
+};
