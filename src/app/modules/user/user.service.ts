91c2c27d3d88b70fe318198ca970f1a56a6dee35
habibullahraju <raju.ra423@gmail.com>
2026-06-06 23:45:48 +0600

    added user zod validation

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,15 +1,36 @@
-import { IUser } from "./user.interface";
+import bcryptjs from "bcryptjs";
+import httpStatus from "http-status-codes";
+import AppError from "../../errorHelpers/appError";
+import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
 
+//create user
 const createUser = async (payload: Partial<IUser>) => {
-  const userData: Partial<IUser> = {};
+  const { email, password, ...rest } = payload;
+  if (!email) {
+    throw new AppError("Email is required", httpStatus.BAD_REQUEST);
+  }
+  const isUserExist = await User.findOne({ email });
+  if (isUserExist) {
+    throw new AppError("User already exist", httpStatus.BAD_REQUEST);
+  }
+  const hashPassword = await bcryptjs.hash(password as string, 10);
+  const authProvider: IAuthProvider = {
+    provider: "credentials",
+    providerId: email as string,
+  };
 
-  if (payload.name !== undefined) userData.name = payload.name;
-  if (payload.email !== undefined) userData.email = payload.email;
-  const user = await User.create(userData);
+  const user = await User.create({
+    email,
+    password: hashPassword,
+    auths: [authProvider],
+    ...rest,
+  });
 
   return user;
 };
+
+// get all users
 const getAllUsers = async () => {
   const users = await User.find();
   return users;
