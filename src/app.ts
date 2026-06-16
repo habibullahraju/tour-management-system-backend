43f69510eb764bc74dfec9a3b9728cb15e0ce696
habibullahraju <raju.ra423@gmail.com>
2026-06-16 23:21:15 +0600

    added passportjs

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import cookiesParser from "cookie-parser";
 import cors from "cors";
 import express, { Request, Response } from "express";
+import passport from "passport";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import { notFound } from "./app/middlewares/notFound";
 import { router } from "./app/routes";
 const app = express();
 //middleware
+app.use(passport.initialize());
 app.use(express.json());
 app.use(cors());
 app.use(cookiesParser());
